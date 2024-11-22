3614a958ccc4e6d70b2a7172e7f30a29b718793f
LuiBastos23 <ybastosdealmeida@gmail.com>
2024-11-22 15:52:44 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Pokemon', 
+    'Qual regiáo com mais pokemon registrados?',
+    'Unova'
+)
+
+criaCartao(
+    'Pokemon', 
+    'Qual personagem da 5 gera;áo consegue entender os Pokemon?',
+    'Tempo e distância são inversamente proporcionais'
+)
+
+criaCartao(
+    'Pokemon', 
+    'Quais os tres tipos iniciais de toda regiäo?',
+    'Agua, Fogo e Grama'
+)
+
+criaCartao(
+    'RDR(1 e 2)',
+    'Quais os nomes dos dois protagonistas?',
+    'John Marston e Arthur Morgan'
+)
+
+criaCartao(
+    'RDR(1 e 2)',
+    'Qual o traidor do RDR2?',
+    'Micah Bell'
+)
+
+criaCartao(
+    'RDR(1 e 2)',
+    'Qual dos Protagonista saca a arma mais rapido?',
+    'Arthur Morgan'
+)
+
+criaCartao(
+    `Bleach`,
+    `Qual nome da Zanpakuto do Ichigo?`,
+    `Zangetsu`
+)
+
+criaCartao(
+    `Bleach`,
+    `Qual nome do Sexto espada de Bleach?`,
+    `Grimmjow Jaegerjaquez`
+)
+
+criaCartao(
+    `Bleach`,
+    `Cor do cabelo do protagonista?`,
+    `Ruivo`
+)
